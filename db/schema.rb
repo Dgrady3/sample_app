45d80b03bd1a22d674601b447da29287a9f0ed73
Dgrady3 <joegrady1@gmail.com>
2014-11-28 21:41:24 -0500

    Email tests pass and are validated for uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141123010512) do
+ActiveRecord::Schema.define(version: 20141129023549) do
 
   create_table "users", force: true do |t|
     t.string   "name"
