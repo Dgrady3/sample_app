0d222d9e2aa6fc4d5832b75815760524e90d083d
Dgrady3 <joegrady1@gmail.com>
2014-12-03 22:37:37 -0500

    Added flash tester

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       # Log the user in and redirect to the user's show page.
     else
       # Create an error message.
-      render 'new'
+        flash[:danger] = 'Invalid email/password combination' # Not quite right!
     end
   end
 
