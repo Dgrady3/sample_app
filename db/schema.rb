e6e26ae62bca87376a7462d1127fc6556d537246
Dgrady3 <joegrady1@gmail.com>
2014-12-08 18:47:17 -0500

    Added methods to user class to help digest remember tokens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129024432) do
+ActiveRecord::Schema.define(version: 20141208234006) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141129024432) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
